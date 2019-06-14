7ee696de162de02f9f75786b4c3c2499c01165ea
TKOFFICE\huxw <huxw@mail.jj.cn>
2019-06-14 14:16:44 +0800

    1. 初始化项目 2. 配置debug

---
+++ VSCode.md
@@ -0,0 +1,8 @@
+# VSCode 
+
+
+
+# 环境配置
+[Visual Studio Code (vscode) 配置C、C++环境/编写运行C、C++（Windows）【真正的小白版】](https://blog.csdn.net/bat67/article/details/81268581)   
+[VS code C++ tasks.json和launch.json的设置](https://www.jianshu.com/p/776a5fb57fbb)      
+[使用VScode运行调试C/C++](https://jingyan.baidu.com/article/7e4409536460c62fc0e2eff2.html)  
---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+int main(int argc, char *args[])
+{
+    printf("hello world!\n");
+    printf("Good !\n");
+
+    std::string  str = "Now is the time...";
+
+
+    std::cout << str << std::endl;
+    // std::cout << std::endl ; 
+
+    system("pause");
+}
